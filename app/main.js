de60809f88614401cc4198d16fdf9e69ce5933f6
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-01-27 01:03:35 +0000

    Functional Pointer (Mouse/Touch) Input and Keyboard Input

--- app/main.js
+++ app/main.js
@@ -28,6 +28,7 @@ var App = function () {
     App.STATE_PLAYING = 1;
     App.FRAMES_PER_SECOND = 30;
     App.COLOUR_SHADOW = "rgba(128,128,128,0.5)";
+    App.MAX_KEYS = 256;
     //--------------------------------
 
     //--------------------------------
@@ -40,29 +41,47 @@ var App = function () {
 
     this.state = App.STATE_START;
     this.player = new Actor(this.width / 2, this.height / 2);
+    this.keys = new Array(App.MAX_KEYS);
     //--------------------------------
 
     //--------------------------------
-    this.inputStart = { x: 0, y: 0 };
-    this.inputNow = { x: 0, y: 0 };
-    this.inputState = App.INPUT_IDLE;
-    this.inputDuration = 0;
+    for (var i = 0; i < this.keys.length; i++) {
+      this.keys[i] = {
+        state: App.INPUT_IDLE,
+        duration: 0
+      };
+    }
+    this.pointer = {
+      start: { x: 0, y: 0 },
+      now: { x: 0, y: 0 },
+      state: App.INPUT_IDLE,
+      duration: 0
+    };
     //--------------------------------
 
     //--------------------------------
     if ("onmousedown" in this.canvas && "onmousemove" in this.canvas && "onmouseup" in this.canvas) {
-      this.canvas.onmousedown = this.canvasOnPointerStart.bind(this);
-      this.canvas.onmousemove = this.canvasOnPointerMove.bind(this);
-      this.canvas.onmouseup = this.canvasOnPointerEnd.bind(this);
+      this.canvas.onmousedown = this.onPointerStart.bind(this);
+      this.canvas.onmousemove = this.onPointerMove.bind(this);
+      this.canvas.onmouseup = this.onPointerEnd.bind(this);
     }
 
     if ("ontouchstart" in this.canvas && "ontouchmove" in this.canvas && "ontouchend" in this.canvas && "ontouchcancel" in this.canvas) {
-      this.canvas.ontouchstart = this.canvasOnPointerStart.bind(this);
-      this.canvas.ontouchmove = this.canvasOnPointerMove.bind(this);
-      this.canvas.ontouchend = this.canvasOnPointerEnd.bind(this);
-      this.canvas.ontouchcancel = this.canvasOnPointerEnd.bind(this);
+      this.canvas.ontouchstart = this.onPointerStart.bind(this);
+      this.canvas.ontouchmove = this.onPointerMove.bind(this);
+      this.canvas.ontouchend = this.onPointerEnd.bind(this);
+      this.canvas.ontouchcancel = this.onPointerEnd.bind(this);
     }
+    //--------------------------------
 
+    //--------------------------------
+    if ("onkeydown" in window && "onkeyup" in window) {
+      window.onkeydown = this.onKeyDown.bind(this);
+      window.onkeyup = this.onKeyUp.bind(this);
+    }
+    //--------------------------------
+
+    //--------------------------------
     this.runCycle = setInterval(this.run.bind(this), 1 / App.FRAMES_PER_SECOND);
     //--------------------------------
   }
@@ -75,8 +94,22 @@ var App = function () {
       this.context.clearRect(0, 0, this.width, this.height);
 
       if (this.player) {
-        this.player.x = this.inputNow.x;
-        this.player.y = this.inputNow.y;
+        if (this.pointer.state == App.INPUT_ACTIVE) {
+          this.player.x = this.pointer.now.x;
+          this.player.y = this.pointer.now.y;
+        }
+
+        if (this.keys[KeyCodes.LEFT].state == App.INPUT_ACTIVE && this.keys[KeyCodes.RIGHT].state != App.INPUT_ACTIVE) {
+          this.player.x -= 2;
+        } else if (this.keys[KeyCodes.LEFT].state != App.INPUT_ACTIVE && this.keys[KeyCodes.RIGHT].state == App.INPUT_ACTIVE) {
+          this.player.x += 2;
+        }
+
+        if (this.keys[KeyCodes.UP].state == App.INPUT_ACTIVE && this.keys[KeyCodes.DOWN].state != App.INPUT_ACTIVE) {
+          this.player.y -= 2;
+        } else if (this.keys[KeyCodes.UP].state != App.INPUT_ACTIVE && this.keys[KeyCodes.DOWN].state == App.INPUT_ACTIVE) {
+          this.player.y += 2;
+        }
 
         this.context.fillStyle = App.COLOUR_SHADOW;
         this.context.beginPath();
@@ -84,42 +117,40 @@ var App = function () {
         this.context.fill();
         this.context.closePath();
 
-        this.console.innerHTML = this.inputNow.x + ',' + this.inputNow.y;
+        //this.console.innerHTML = this.pointer.now.x + ',' + this.pointer.now.y;
+        this.console.innerHTML = this.keys[KeyCodes.DOWN].state + ':' + this.keys[KeyCodes.DOWN].duration;
       }
     }
 
     //----------------------------------------------------------------
 
   }, {
-    key: "canvasOnPointerStart",
-    value: function canvasOnPointerStart(e) {
-      console.log('canvasOnPointerStart()');
-      this.inputState = App.INPUT_ACTIVE;
-      this.inputDuration = 1;
-      this.inputStart = this.getInputXY(e);
-      this.inputNow = this.inputStart;
+    key: "onPointerStart",
+    value: function onPointerStart(e) {
+      this.pointer.state = App.INPUT_ACTIVE;
+      this.pointer.duration = 1;
+      this.pointer.start = this.getPointerXY(e);
+      this.pointer.now = this.pointer.start;
       return Utility.stopEvent(e);
     }
   }, {
-    key: "canvasOnPointerMove",
-    value: function canvasOnPointerMove(e) {
-      console.log('canvasOnPointerMove()');
-      if (this.inputState === App.INPUT_ACTIVE) {
-        this.inputNow = this.getInputXY(e);
+    key: "onPointerMove",
+    value: function onPointerMove(e) {
+      if (this.pointer.state === App.INPUT_ACTIVE) {
+        this.pointer.now = this.getPointerXY(e);
       }
       return Utility.stopEvent(e);
     }
   }, {
-    key: "canvasOnPointerEnd",
-    value: function canvasOnPointerEnd(e) {
-      console.log('canvasOnPointerEnd()');
-      this.inputState = App.INPUT_ENDED;
-      //this.inputNow = this.getInputXY(e);
+    key: "onPointerEnd",
+    value: function onPointerEnd(e) {
+      this.pointer.state = App.INPUT_ENDED;
+      //this.pointer.now = this.getPointerXY(e);
       return Utility.stopEvent(e);
     }
   }, {
-    key: "getInputXY",
-    value: function getInputXY(e) {
+    key: "getPointerXY",
+    value: function getPointerXY(e) {
       var boundingBox = this.canvas.getBoundingClientRect ? this.canvas.getBoundingClientRect() : { left: 0, top: 0 };
       var clientX = 0;
       var clientY = 0;
@@ -133,6 +164,45 @@ var App = function () {
       var inputX = (clientX - boundingBox.left) * this.width / boundingBox.width;
       var inputY = (clientY - boundingBox.top) * this.height / boundingBox.height;
       return { x: inputX, y: inputY };
+    }
+
+    //----------------------------------------------------------------
+
+  }, {
+    key: "onKeyDown",
+    value: function onKeyDown(e) {
+      var keyCode = this.getKeyCode(e);
+      if (keyCode > 0 && keyCode < App.MAX_KEYS) {
+        this.keys[keyCode].state = App.INPUT_ACTIVE;
+        this.keys[keyCode].duration = 1;
+      } //if keyCode == 0, there's an error.
+    }
+  }, {
+    key: "onKeyUp",
+    value: function onKeyUp(e) {
+      var keyCode = this.getKeyCode(e);
+      if (keyCode > 0 && keyCode < App.MAX_KEYS) {
+        this.keys[keyCode].state = App.INPUT_ENDED;
+      } //if keyCode == 0, there's an error.
+    }
+  }, {
+    key: "getKeyCode",
+    value: function getKeyCode(e) {
+      //KeyboardEvent.keyCode is the most reliable identifier for a keyboard event
+      //at the moment, but unfortunately it's being deprecated.
+      if (e.keyCode) {
+        return e.keyCode;
+      }
+
+      //KeyboardEvent.code and KeyboardEvent.key are the 'new' standards, but it's
+      //far from being standardised between browsers.
+      if (e.code && KeyValues[e.code]) {
+        return KeyValues[e.code];
+      } else if (e.key && KeyValues[e.key]) {
+        return KeyValues[e.key];
+      }
+
+      return 0;
     }
   }]);
 
@@ -172,6 +242,26 @@ var Utility = {
     e.cancelBubble = true;
     return false;
   }
+};
+
+var KeyCodes = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  ENTER: 13
+};
+
+var KeyValues = {
+  "ArrowLeft": KeyCodes.LEFT,
+  "Left": KeyCodes.LEFT,
+  "ArrowUp": KeyCodes.UP,
+  "Up": KeyCodes.UP,
+  "ArrowDown": KeyCodes.DOWN,
+  "Down": KeyCodes.DOWN,
+  "ArrowRight": KeyCodes.RIGHT,
+  "Right": KeyCodes.RIGHT,
+  "Enter": KeyCodes.ENTER
 };
 
 function ImageAsset(url) {
--- src/main.js
+++ src/main.js
@@ -20,6 +20,7 @@ class App {
     App.STATE_PLAYING = 1;
     App.FRAMES_PER_SECOND = 30;
     App.COLOUR_SHADOW = "rgba(128,128,128,0.5)";
+    App.MAX_KEYS = 256;
     //--------------------------------
     
     //--------------------------------
@@ -32,31 +33,49 @@ class App {
 
     this.state = App.STATE_START;    
     this.player = new Actor(this.width / 2, this.height / 2);
+    this.keys = new Array(App.MAX_KEYS);
     //--------------------------------
     
     //--------------------------------
-    this.inputStart = { x: 0, y: 0 };
-    this.inputNow = { x: 0, y: 0 };
-    this.inputState = App.INPUT_IDLE;
-    this.inputDuration = 0;
+    for (let i = 0; i < this.keys.length; i++) {
+      this.keys[i] = {
+        state: App.INPUT_IDLE,
+        duration: 0
+      };
+    }
+    this.pointer = {
+      start: { x: 0, y: 0 },
+      now: { x: 0, y: 0 },
+      state: App.INPUT_IDLE,
+      duration: 0
+    }
     //--------------------------------
     
     //--------------------------------
     if ("onmousedown" in this.canvas && "onmousemove" in this.canvas &&
         "onmouseup" in this.canvas) {
-      this.canvas.onmousedown = this.canvasOnPointerStart.bind(this);
-      this.canvas.onmousemove = this.canvasOnPointerMove.bind(this);
-      this.canvas.onmouseup = this.canvasOnPointerEnd.bind(this);
+      this.canvas.onmousedown = this.onPointerStart.bind(this);
+      this.canvas.onmousemove = this.onPointerMove.bind(this);
+      this.canvas.onmouseup = this.onPointerEnd.bind(this);
     }
     
     if ("ontouchstart" in this.canvas && "ontouchmove" in this.canvas &&
         "ontouchend" in this.canvas && "ontouchcancel" in this.canvas) {
-      this.canvas.ontouchstart = this.canvasOnPointerStart.bind(this);
-      this.canvas.ontouchmove = this.canvasOnPointerMove.bind(this);
-      this.canvas.ontouchend = this.canvasOnPointerEnd.bind(this);
-      this.canvas.ontouchcancel = this.canvasOnPointerEnd.bind(this);
+      this.canvas.ontouchstart = this.onPointerStart.bind(this);
+      this.canvas.ontouchmove = this.onPointerMove.bind(this);
+      this.canvas.ontouchend = this.onPointerEnd.bind(this);
+      this.canvas.ontouchcancel = this.onPointerEnd.bind(this);
     }
+    //--------------------------------
+    
+    //--------------------------------
+    if ("onkeydown" in window && "onkeyup" in window) {
+      window.onkeydown = this.onKeyDown.bind(this);
+      window.onkeyup = this.onKeyUp.bind(this);
+    }
+    //--------------------------------
       
+    //--------------------------------
     this.runCycle = setInterval(this.run.bind(this), 1 / App.FRAMES_PER_SECOND);
     //--------------------------------
     
@@ -68,46 +87,62 @@ class App {
     this.context.clearRect(0, 0, this.width, this.height);
     
     if (this.player) {
-      this.player.x = this.inputNow.x;
-      this.player.y = this.inputNow.y;
+      if (this.pointer.state == App.INPUT_ACTIVE) {
+        this.player.x = this.pointer.now.x;
+        this.player.y = this.pointer.now.y;
+      } 
       
+      if (this.keys[KeyCodes.LEFT].state == App.INPUT_ACTIVE &&
+          this.keys[KeyCodes.RIGHT].state != App.INPUT_ACTIVE) {
+        this.player.x -= 2
+      } else if (this.keys[KeyCodes.LEFT].state != App.INPUT_ACTIVE &&
+          this.keys[KeyCodes.RIGHT].state == App.INPUT_ACTIVE) {
+        this.player.x += 2
+      }
+      
+      if (this.keys[KeyCodes.UP].state == App.INPUT_ACTIVE &&
+          this.keys[KeyCodes.DOWN].state != App.INPUT_ACTIVE) {
+        this.player.y -= 2
+      } else if (this.keys[KeyCodes.UP].state != App.INPUT_ACTIVE &&
+          this.keys[KeyCodes.DOWN].state == App.INPUT_ACTIVE) {
+        this.player.y += 2
+      }
+    
       this.context.fillStyle = App.COLOUR_SHADOW;
       this.context.beginPath();
       this.context.arc(this.player.x, this.player.y, this.player.size / 2, 0, 2 * Math.PI);
       this.context.fill();
       this.context.closePath();
       
-      this.console.innerHTML = this.inputNow.x + ',' + this.inputNow.y;
+      //this.console.innerHTML = this.pointer.now.x + ',' + this.pointer.now.y;
+      this.console.innerHTML = this.keys[KeyCodes.DOWN].state + ':' + this.keys[KeyCodes.DOWN].duration;
     }
   }
   
   //----------------------------------------------------------------
   
-  canvasOnPointerStart(e) {
-    console.log('canvasOnPointerStart()');
-    this.inputState = App.INPUT_ACTIVE;
-    this.inputDuration = 1;
-    this.inputStart = this.getInputXY(e);
-    this.inputNow = this.inputStart;
+  onPointerStart(e) {
+    this.pointer.state = App.INPUT_ACTIVE;
+    this.pointer.duration = 1;
+    this.pointer.start = this.getPointerXY(e);
+    this.pointer.now = this.pointer.start;
     return Utility.stopEvent(e);
   }
   
-  canvasOnPointerMove(e) {
-    console.log('canvasOnPointerMove()');
-    if (this.inputState === App.INPUT_ACTIVE) {
-      this.inputNow = this.getInputXY(e);
+  onPointerMove(e) {
+    if (this.pointer.state === App.INPUT_ACTIVE) {
+      this.pointer.now = this.getPointerXY(e);
     }
     return Utility.stopEvent(e);
   }
   
-  canvasOnPointerEnd(e) {
-    console.log('canvasOnPointerEnd()');
-    this.inputState = App.INPUT_ENDED;
-    //this.inputNow = this.getInputXY(e);
+  onPointerEnd(e) {
+    this.pointer.state = App.INPUT_ENDED;
+    //this.pointer.now = this.getPointerXY(e);
     return Utility.stopEvent(e);
   }
   
-  getInputXY(e) {
+  getPointerXY(e) {
     let boundingBox = (this.canvas.getBoundingClientRect)
       ? this.canvas.getBoundingClientRect()
       : { left: 0, top: 0 };     
@@ -116,13 +151,49 @@ class App {
     if (e.clientX && e.clientY) {
       clientX = e.clientX;
       clientY = e.clientY;
-    } else if (e.touches && e.touches.length > 0 && e.touches[0].clientX && e.touches[0].clientY) {
+    } else if (e.touches && e.touches.length > 0 && e.touches[0].clientX &&
+        e.touches[0].clientY) {
       clientX = e.touches[0].clientX;
       clientY = e.touches[0].clientY;
     }
     let inputX = (clientX - boundingBox.left) * this.width / boundingBox.width;
     let inputY = (clientY - boundingBox.top) * this.height / boundingBox.height;
     return { x: inputX, y: inputY };
+  }
+
+  //----------------------------------------------------------------
+  
+  onKeyDown(e) {
+    let keyCode = this.getKeyCode(e);    
+    if (keyCode > 0 && keyCode < App.MAX_KEYS) {
+      this.keys[keyCode].state = App.INPUT_ACTIVE;
+      this.keys[keyCode].duration = 1;
+    }  //if keyCode == 0, there's an error.
+  }
+  
+  onKeyUp(e) {
+    let keyCode = this.getKeyCode(e);    
+    if (keyCode > 0 && keyCode < App.MAX_KEYS) {
+      this.keys[keyCode].state = App.INPUT_ENDED;
+    }  //if keyCode == 0, there's an error.
+  }
+  
+  getKeyCode(e) {
+    //KeyboardEvent.keyCode is the most reliable identifier for a keyboard event
+    //at the moment, but unfortunately it's being deprecated.
+    if (e.keyCode) { 
+      return e.keyCode;
+    }
+    
+    //KeyboardEvent.code and KeyboardEvent.key are the 'new' standards, but it's
+    //far from being standardised between browsers.
+    if (e.code && KeyValues[e.code]) {
+      return KeyValues[e.code]
+    } else if (e.key && KeyValues[e.key]) {
+      return KeyValues[e.key]
+    }
+    
+    return 0;
   }
 }
 //==============================================================================
@@ -157,6 +228,26 @@ var Utility = {
     e.cancelBubble = true;
     return false;
   }
+}
+
+var KeyCodes = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  ENTER: 13
+}
+
+var KeyValues = {
+  "ArrowLeft": KeyCodes.LEFT,
+  "Left": KeyCodes.LEFT,
+  "ArrowUp": KeyCodes.UP,
+  "Up": KeyCodes.UP,
+  "ArrowDown": KeyCodes.DOWN,
+  "Down": KeyCodes.DOWN,
+  "ArrowRight": KeyCodes.RIGHT,
+  "Right": KeyCodes.RIGHT,
+  "Enter": KeyCodes.ENTER
 }
 
 function ImageAsset(url) {
